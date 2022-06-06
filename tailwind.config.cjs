6ca902800f7b39e3c7051ff107f6541ef0d39629
miracleonyenma <miracleficient@gmail.com>
2022-06-06 13:35:03 +0100

    add cart section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Anek Gurmukhi',
+          'Space Grotesk',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
