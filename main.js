549daf642d36b138c6a8fbf43cbfa8bded28edb5
miracleonyenma <42788603+miracleonyenma@users.noreply.github.com>
2023-05-18 23:09:09 +0100

    1 file modified

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
 import './style.css';
-import {shop100Pay} from "@100pay-hq/checkout"
+import { shop100Pay } from '@100pay-hq/checkout';
 
 const paymentForm = document.querySelector('#paymentForm');
 
 paymentForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  let firstname = paymentForm.firstName.value
-  let lastname = paymentForm.lastName.value
-  let emailaddress = paymentForm.emailAddress.value
-  let currency = paymentForm.currency.value
-  let amount = paymentForm.amount.value
-  let phone = paymentForm.phone.value
+  let firstname = paymentForm.firstName.value;
+  let lastname = paymentForm.lastName.value;
+  let emailaddress = paymentForm.emailAddress.value;
+  let currency = paymentForm.currency.value;
+  let amount = paymentForm.amount.value;
+  let phone = paymentForm.phone.value;
 
-  console.log({firstname, lastname, emailaddress, currency, amount, phone})
+  console.log({ firstname, lastname, emailaddress, currency, amount, phone });
 
   shop100Pay.setup({
     ref_id: `${Math.floor(Math.random() * 1000000000 + 1)}`,
-    api_key: "LIVE;PK;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjYyOGQyMDA1N2U0YmQyMDAyZDg5YTk3NyIsInVzZXJJZCI6IjYyOGQxZjEzN2U0YmQyMDAyZDg5YTk1MCIsImlhdCI6MTY1MzQxNTk0MX0.4a9Ov5x5MEmbIIO4ebRYG_9nH4BBxa7Fxo2tcC7BO14",
+    api_key:
+      'LIVE;PK;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjYyOGIxYzdlMTlhYzUyMDAyYzU4ODQ2YSIsInVzZXJJZCI6IjYxY2VjZDNjNWU0NjM0MDAyZTg3YmU5NSIsImlhdCI6MTY1MzI4Mzk2Nn0.Y54T8kQxaAOvZjguvyRZKc7D_v2xpU0pSHBVwTNcRL8',
     customer: {
-      user_id: "1", // optional
+      user_id: '1', // optional
       name: `${firstname} ${lastname}`,
       phone,
-      email: emailaddress
+      email: emailaddress,
     },
     billing: {
       amount,
       currency, // or any other currency supported by 100pay
-      description: "Test Payment",
-      country: "USA",
+      description: 'Test Payment',
+      country: 'USA',
       vat: 10, //optional
-      pricing_type: "fixed_price" // or partial
+      pricing_type: 'fixed_price', // or partial
     },
     metadata: {
-      is_approved: "yes",
-      order_id: "OR2", // optional
-      charge_ref: "REF" // optional, you can add more fields
+      is_approved: 'yes',
+      order_id: 'OR2', // optional
+      charge_ref: 'REF', // optional, you can add more fields
     },
-    call_back_url: "http://localhost:8000/verifyorder/",
-    onClose: msg => {
-      alert("User closed payment modal.");
+    call_back_url: 'http://localhost:8000/verifyorder/',
+    onClose: (msg) => {
+      alert('User closed payment modal.');
     },
-    callback: reference => {
+    callback: (reference) => {
       alert(`Transaction successful with id: ${reference}`);
     },
-    onError: error => {
-        console.log(error)
-        alert("Sorry something went wrong pls try again.")
-    }
-
-  })
-
+    onError: (error) => {
+      console.log(error);
+      alert('Sorry something went wrong pls try again.');
+    },
+  });
 });
